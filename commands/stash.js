43d8f249134350a5f0ec9d72a7a9fd632c41ebe0
Prem Raghvani <69864865+premraghvani@users.noreply.github.com>
2026-06-01 14:46:53 +0100

    implemented new page loading systems

--- commands/stash.js
+++ commands/stash.js
@@ -1,10 +1,11 @@
-const window = require("../STASHWINDOW.json") 
+const config = require("../config.json")
+const { getS3Item } = require("./awsFunctions")
+const MarkdownIt = require("markdown-it");
+const md = new MarkdownIt();
 const fs = require("fs");
 
 // handler
 async function stashPage(event){
-    const openTime = new Date(window.open);
-    const closeTime = new Date(window.close);
     const timeNow = new Date();
     
     let returnBody = {
@@ -14,17 +15,73 @@ async function stashPage(event){
         }
     }
 
-    if(openTime.getTime() <= timeNow.getTime() && closeTime.getTime() >= timeNow.getTime()){
-        // open case
-        returnBody.body = fs.readFileSync(`assets/html/stash.html`).toString();
-    } else {
-        // closed case
-        returnBody.body = fs.readFileSync(`assets/html/stashClosed.html`).toString();
+    // load the markdown file
+    let inputMarkdown = "";
+    try {
+        let tmpHash = await getS3Item(config.buckets.operational, `pages/stash.md`);
+        inputMarkdown = tmpHash.toString();
+    } catch(err){
+        console.log(err)
+        return false;
     }
 
-    // appends current data
-    returnBody.body = returnBody.body.replace(/{{openDate}}/g,window.open)
-    returnBody.body = returnBody.body.replace(/{{closeDate}}/g,window.close)
+    // get the parameters
+    const lines = inputMarkdown.split("\n");
+    const openParam = lines[0].substring("!open=".length).trim();
+    const closeParam = lines[1].substring("!close=".length).trim();
+
+    // remove the metadata lines
+    inputMarkdown = lines.slice(2).join("\n");
+    
+    // null parameters
+    if(!openParam){openParam = "Jan 01, 1970 00:00:00"}
+    if(!closeParam){closeParam = "Jan 01, 1970 00:00:00"}
+
+    // parameterisation
+    const openTime = new Date(openParam);
+    const closeTime = new Date(closeParam);
+
+    // null
+    if (isNaN(openTime.getTime())) {
+        openTime = new Date("Jan 01, 1970 00:00:00");
+        openParam = "Jan 01, 1970 00:00:00"
+    }
+    if (isNaN(closeTime.getTime())) {
+        closeTime = new Date("Jan 01, 1970 00:00:00");
+        closeParam = "Jan 01, 1970 00:00:00"
+    }
+    
+    // template setup
+    returnBody.body = fs.readFileSync(`assets/html/template.html`).toString();
+
+    // determination of view
+    if(openTime.getTime() <= timeNow.getTime() && closeTime.getTime() >= timeNow.getTime()){
+        // operating window - inject markdown
+        
+        // render
+        let inputHtml = md.render(inputMarkdown)
+            .replace(/&lt;/g,"<")
+            .replace(/&gt;/g,">")
+            .replace(/&quot;/g,"\"");
+
+        returnBody.body = returnBody.body.replace(/{{content}}/g,inputHtml);
+
+    } else {
+        // closed window - no stash!
+        returnBody.body = returnBody.body.replace(/{{content}}/g,`<p>
+                <b>Remark.</b> The stash ordering is currently closed.
+                <br><br>
+                Open: {{openDate}} (UTC+0)<br>
+                Closed: {{closeDate}} (UTC+0)
+            </p>`);
+    }
+
+    // appends parameters
+    returnBody.body = returnBody.body.replace(/{{openDate}}/g,openParam);
+    returnBody.body = returnBody.body.replace(/{{closeDate}}/g,closeParam);
+
+    // appends name
+    returnBody.body = returnBody.body.replace(/{{pageName}}/g,"Stash");
 
     return returnBody
 }
